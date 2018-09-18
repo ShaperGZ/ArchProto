06b4a50d307379f1852d88f836b77e74f2b8a7ef
shapergz <lisi@lwkp.com.cn>
2018-09-18 18:32:34 +0800

    trying to print the ObserverManager, WIP

--- observer_manager.rb
+++ observer_manager.rb
@@ -1,6 +1,49 @@
 $ObserverManager = Hash.new if $ObserverManager == nil
 
+$nameMatch=[
+    Sketchup::EntitiesObserver,
+    Sketchup::EntityObserver,
+    Sketchup::InstanceObserver,
+    Sketchup::SelectionObserver
+    ]
+
+
 class ObserverManger
+  def self.Print()
+    print self.Format()
+  end
+
+  def self.Format()
+    # name       | ens | ent | ins | sel |
+    #-------------------------------------
+    # selection  | 0   | 0   | 0   | 1   |
+    # 321232     | 2   | 1   | 0   | 0   |
+    # 432554     | 2   | 2   | 0   | 0   |
+    #
+
+    output=""
+    $ObserverManager.each{|k,v|
+      txt = "#{k} "
+      counts=self.Count(v)
+      counts.each{|c|
+        txt += " | #{c} "
+      }
+      output += txt + "\n"
+    }
+    return output;
+  end
+
+  def self.Count(observers)
+    counts=[0]*nameMatch.size
+    observers.each{|ob|
+      i=$nameMatch.index(ob.class)
+      if i>=0 and i<$nameMatch.size
+          counts[i]+=1
+      end
+    }
+    return counts
+  end
+
   def self.Add(gp, id)
     if $ObserverManager.key? gp
       $ObserverManager[gp] << id
